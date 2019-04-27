2b827db5404e866360997646a6787ca59b1a6e8d
pegasus1982 <gdm19920108@gmail.com>
2019-04-27 11:13:23 +0800

    update cilindro 2 model

--- assets/js/script.js
+++ assets/js/script.js
@@ -761,9 +761,9 @@ document.getElementById('btn-find').addEventListener('click',function(){
     if(!isNaN(numTube)){
         checkCilindro_01(numCylinder, numQuadrant, numSection, numTube);
     }
-    // else if(colorlist.includes(numTube)){
-    //     checkCilindro_02(numCylinder, numQuadrant, numTube, bChecked_Cylinder,bChecked_Quadrant);
-    // }
+    else if(colorlist.includes(numTube)){
+        checkCilindro_02(numCylinder, numQuadrant, numTube, bChecked_Cylinder,bChecked_Quadrant);
+    }
     // else{
     //     checkCilindro_03(numCylinder, numQuadrant, numTube, bChecked_Cylinder,bChecked_Quadrant,bChecked_Tube);
     //     checkCilindro_04(numCylinder, numQuadrant, numTube, bChecked_Cylinder,bChecked_Quadrant,bChecked_Tube);
